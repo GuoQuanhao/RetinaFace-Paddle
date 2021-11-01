efa8cb144281124ca8372c4976ff952bb77ebfbe
QuanHao Guo <49911294+GuoQuanhao@users.noreply.github.com>
2021-11-01 10:55:05 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -74,82 +74,82 @@ if __name__ == '__main__':
     resize = 1
 
     # testing begin
-    for i in range(100):
-        image_path = "./curve/test.jpg"
-        img_raw = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    image_path = "./curve/test.jpg"
+    img_raw = cv2.imread(image_path, cv2.IMREAD_COLOR)
 
-        img = np.float32(img_raw)
+    img = np.float32(img_raw)
 
-        im_height, im_width, _ = img.shape
-        scale = paddle.to_tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
-        img -= (104, 117, 123)
-        img = img.transpose(2, 0, 1)
-        img = paddle.to_tensor(img).unsqueeze(0)
+    im_height, im_width, _ = img.shape
+    scale = paddle.to_tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
+    img -= (104, 117, 123)
+    img /= (57.1,57.4,58.4)
+    img = img.transpose(2, 0, 1)
+    img = paddle.to_tensor(img).unsqueeze(0)
 
-        tic = time.time()
-        loc, conf, landms = net(img)  # forward pass
-        print('net forward time: {:.4f}'.format(time.time() - tic))
+    tic = time.time()
+    loc, conf, landms = net(img)  # forward pass
+    print('net forward time: {:.4f}'.format(time.time() - tic))
 
-        priorbox = PriorBox(cfg, image_size=(im_height, im_width))
-        priors = priorbox.forward()
-        prior_data = priors
-        boxes = decode(loc.squeeze(0), prior_data, cfg['variance'])
-        boxes = boxes * scale / resize
-        boxes = boxes.cpu().numpy()
-        scores = conf.squeeze(0).cpu().numpy()[:, 1]
-        landms = decode_landm(landms.squeeze(0), prior_data, cfg['variance'])
-        scale1 = paddle.to_tensor([img.shape[3], img.shape[2], img.shape[3], img.shape[2],
-                               img.shape[3], img.shape[2], img.shape[3], img.shape[2],
-                               img.shape[3], img.shape[2]])
-        landms = landms * scale1 / resize
-        landms = landms.cpu().numpy()
+    priorbox = PriorBox(cfg, image_size=(im_height, im_width))
+    priors = priorbox.forward()
+    prior_data = priors
+    boxes = decode(loc.squeeze(0), prior_data, cfg['variance'])
+    boxes = boxes * scale / resize
+    boxes = boxes.cpu().numpy()
+    scores = conf.squeeze(0).cpu().numpy()[:, 1]
+    landms = decode_landm(landms.squeeze(0), prior_data, cfg['variance'])
+    scale1 = paddle.to_tensor([img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                           img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                           img.shape[3], img.shape[2]])
+    landms = landms * scale1 / resize
+    landms = landms.cpu().numpy()
 
-        # ignore low scores
-        inds = np.where(scores > args.confidence_threshold)[0]
-        boxes = boxes[inds]
-        landms = landms[inds]
-        scores = scores[inds]
+    # ignore low scores
+    inds = np.where(scores > args.confidence_threshold)[0]
+    boxes = boxes[inds]
+    landms = landms[inds]
+    scores = scores[inds]
 
-        # keep top-K before NMS
-        order = scores.argsort()[::-1][:args.top_k]
-        boxes = boxes[order]
-        landms = landms[order]
-        scores = scores[order]
+    # keep top-K before NMS
+    order = scores.argsort()[::-1][:args.top_k]
+    boxes = boxes[order]
+    landms = landms[order]
+    scores = scores[order]
 
-        # do NMS
-        dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
-        keep = py_cpu_nms(dets, args.nms_threshold)
-        # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
-        dets = dets[keep, :]
-        landms = landms[keep]
+    # do NMS
+    dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+    keep = py_cpu_nms(dets, args.nms_threshold)
+    # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
+    dets = dets[keep, :]
+    landms = landms[keep]
 
-        # keep top-K faster NMS
-        dets = dets[:args.keep_top_k, :]
-        landms = landms[:args.keep_top_k, :]
+    # keep top-K faster NMS
+    dets = dets[:args.keep_top_k, :]
+    landms = landms[:args.keep_top_k, :]
 
-        dets = np.concatenate((dets, landms), axis=1)
+    dets = np.concatenate((dets, landms), axis=1)
 
-        # show image
-        if args.save_image:
-            for b in dets:
-                if b[4] < args.vis_thres:
-                    continue
-                text = "{:.4f}".format(b[4])
-                b = list(map(int, b))
-                cv2.rectangle(img_raw, (b[0], b[1]), (b[2], b[3]), (0, 0, 255), 2)
-                cx = b[0]
-                cy = b[1] + 12
-                cv2.putText(img_raw, text, (cx, cy),
-                            cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
+    # show image
+    if args.save_image:
+        for b in dets:
+            if b[4] < args.vis_thres:
+                continue
+            text = "{:.4f}".format(b[4])
+            b = list(map(int, b))
+            cv2.rectangle(img_raw, (b[0], b[1]), (b[2], b[3]), (0, 0, 255), 2)
+            cx = b[0]
+            cy = b[1] + 12
+            cv2.putText(img_raw, text, (cx, cy),
+                        cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
 
-                # landms
-                cv2.circle(img_raw, (b[5], b[6]), 1, (0, 0, 255), 4)
-                cv2.circle(img_raw, (b[7], b[8]), 1, (0, 255, 255), 4)
-                cv2.circle(img_raw, (b[9], b[10]), 1, (255, 0, 255), 4)
-                cv2.circle(img_raw, (b[11], b[12]), 1, (0, 255, 0), 4)
-                cv2.circle(img_raw, (b[13], b[14]), 1, (255, 0, 0), 4)
-            # save image
+            # landms
+            cv2.circle(img_raw, (b[5], b[6]), 1, (0, 0, 255), 4)
+            cv2.circle(img_raw, (b[7], b[8]), 1, (0, 255, 255), 4)
+            cv2.circle(img_raw, (b[9], b[10]), 1, (255, 0, 255), 4)
+            cv2.circle(img_raw, (b[11], b[12]), 1, (0, 255, 0), 4)
+            cv2.circle(img_raw, (b[13], b[14]), 1, (255, 0, 0), 4)
+        # save image
 
-            name = "test.jpg"
-            cv2.imwrite(name, img_raw)
+        name = "test.jpg"
+        cv2.imwrite(name, img_raw)
 
